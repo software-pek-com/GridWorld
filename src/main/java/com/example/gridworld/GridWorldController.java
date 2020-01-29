771e8b52d65c699e5715ec70c8a5c9f1ebfc5e9b
Filip Fodemski <filip@quantifai.xyz>
2020-01-29 14:03:47 +0100

    Initial code. Mostly autogenerated by spring boot wizard (in VS Code). Application and controller are copied from spring boot examples.

---
+++ src/main/java/com/example/gridworld/GridWorldController.java
@@ -0,0 +1,49 @@
+package com.example.gridworld;
+
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class GridWorldController {
+
+	private static String nowAsString() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HHmmss'Z'");
+		Date dateobj = new Date();
+		return dateFormat.format(dateobj);
+	}
+
+	@RequestMapping("/hello")
+	public String index() {
+		String response = "Greetings from Spring Boot! Time now is ";
+		return response + nowAsString();
+	}
+
+	@PutMapping("/save/{magicNumber}")
+	public String saveToFile(@RequestBody String message, @PathVariable Integer magicNumber) 
+		throws Exception {
+
+		String timeStamp = nowAsString();
+		String response = String.format("Saved %d as '%s' [%s].", magicNumber, message, timeStamp);
+		String fileName = String.format("%s-%s.bytes", message, timeStamp);
+		byte[] fileBytes = response.getBytes();
+
+		FileOutputStream file = new FileOutputStream(fileName);
+		file.write(fileBytes);
+		file.close();
+
+		return response;
+	}
+
+	@RequestMapping("/gridworld")
+	public String gridWorld() {
+		return "Greetings from the grid world!";
+	}
+}
